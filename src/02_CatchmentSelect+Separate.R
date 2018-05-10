013ea32b6f5be4f88e4e961fc6930d629fd779a5
Sam <samuelczipper@gmail.com>
2018-05-10 07:52:10 -0700

    Select catchments and calculate baseflow with 7 methods

--- src/02_CatchmentSelect+Separate.R
+++ src/02_CatchmentSelect+Separate.R
@@ -3,8 +3,8 @@
 #' (produced with the script 01_CatchmentGapFill+Stats.R), decide which 
 #' catchments for final analysis, and perform baseflow separation on them.
 
-source("src/paths+packages.R")
-source("src/BaseflowSeparationFunctions.R")
+source(file.path("src", "paths+packages.R"))
+source(file.path("src", "BaseflowSeparationFunctions.R"))
 
 # Decide catchments to keep ----------------------------------------------------
 
@@ -35,7 +35,24 @@ yrs.complete.min <- 10  # Beck et al. (2013, 2016) WRR used 10 years
 # subset summary data
 df.summary <- subset(df.summary, yrs.complete >= yrs.complete.min)
 
-# make plots of retained catchments
+cols.hist <- c("catchment", "yrs.complete", "catchsize", "meanelevation", "aridity", "snowfall_fraction", "SoilGrids1km_sand")
+p.properties.hist <- 
+  df.summary[,cols.hist] %>% 
+  melt(id="catchment") %>% 
+  ggplot(aes(x=value)) +
+  geom_histogram() +
+  facet_wrap(~variable, ncol=3, scales="free",
+             labeller=as_labeller(c("yrs.complete"="Years Data",
+                                    "catchsize"="Catchment Size [km2]",
+                                    "meanelevation"="Mean Elevation [m]",
+                                    "aridity"="Aridity (=PET/P)",
+                                    "snowfall_fraction"="Fraction of Precip as Snow",
+                                    "SoilGrids1km_sand"="Soil Sand Content"))) +
+  scale_y_continuous(name="# of Catchments") +
+  scale_x_continuous(name="Value")
+ggsave(file.path("plots", "CatchmentSelect_HistogramProperties.png"),
+       p.properties.hist, width=8, height=6, units="in")
+
 df.summary$yrs.complete.cut <- cut(df.summary$yrs.complete, c(10,20,30,500), 
                                    labels=c("10-20", "20-30", ">30"), 
                                    include.lowest=F, right=F)
@@ -56,28 +73,12 @@ p.map.yrs <-
 ggsave(file.path("plots", "CatchmentSelect_Map.png"),
        p.map.yrs, width=8, height=6, units="in")
 
-cols.hist <- c("catchment", "yrs.complete", "catchsize", "meanelevation", "aridity", "snowfall_fraction", "SoilGrids1km_sand")
-p.properties.hist <- 
-  df.summary[,cols.hist] %>% 
-  melt(id="catchment") %>% 
-  ggplot(aes(x=value)) +
-  geom_histogram() +
-  facet_wrap(~variable, ncol=3, scales="free",
-             labeller=as_labeller(c("yrs.complete"="Years Data",
-                                    "catchsize"="Catchment Size [km2]",
-                                    "meanelevation"="Mean Elevation [m]",
-                                    "aridity"="Aridity (=PET/P)",
-                                    "snowfall_fraction"="Fraction of Precip as Snow",
-                                    "SoilGrids1km_sand"="Soil Sand Content"))) +
-  scale_y_continuous(name="# of Catchments") +
-  scale_x_continuous(name="Value")
-ggsave(file.path("plots", "CatchmentSelect_HistographProperties.png"),
-       p.properties.hist, width=8, height=6, units="in")
-
 # Perform baseflow separation on each catchment ---------------------------
 
 # df summary columns to save
 df.summary <- subset(df.summary, select=c("catchment", "catchsize"))
+df.summary$recessionConstant <- NaN
+df.summary$BFImax <- NaN
 
 n.catchment <- length(df.summary$catchment)
 start.flag <- T
@@ -107,16 +108,9 @@ for (cat in 1:n.catchment){
   df.cat$year <- year(df.cat$date)
   df.cat$month <- month(df.cat$date)
   
-  # estimate recession constant
+  # estimate recession constant and BFImax
   k <- baseflow_RecessionConstant(df.cat$Q_mm.d, UB_prc=0.95, method="Brutsaert")
-  
-  # figure out BFImax; Eckhardt (2008) uses >10% 0-flow days to define ephemeral
-  # right now, just assuming all have porous aquifers
-  if (sum(df.cat$Q_mm.d==0)/length(df.cat$Q_mm.d) > 0.1){
-    BFImax <- 0.5
-  } else {
-    BFImax <- 0.8
-  }
+  df.summary$recessionConstant[cat] <- k
   
   # perform baseflow separations
   df.cat$HYSEP_fixed <- baseflow_HYSEP(Q = df.cat$Q_mm.d, area_mi2 = area_mi2, method="fixed")
@@ -125,7 +119,14 @@ for (cat in 1:n.catchment){
   df.cat$UKIH <- baseflow_UKIH(Q = df.cat$Q_mm.d, endrule="B")
   df.cat$BFLOW_1pass <- baseflow_BFLOW(Q = df.cat$Q_mm.d, beta=0.925, passes=1)
   df.cat$BFLOW_3pass <- baseflow_BFLOW(Q = df.cat$Q_mm.d, beta=0.925, passes=3)
-  df.cat$Eckhardt <- baseflow_Eckhardt(Q = df.cat$Q_mm.d, BFImax=BFImax, k=k)
+  if (is.finite(k)){
+    if (k>1) stop("Error: k > 1")
+    BFImax <- baseflow_BFImax(df.cat$Q_mm.d, k=k)
+    df.summary$BFImax[cat] <- BFImax
+    df.cat$Eckhardt <- baseflow_Eckhardt(Q = df.cat$Q_mm.d, BFImax=BFImax, k=k)
+  } else {
+    df.cat$Eckhardt <- NaN
+  }
   
   # save gap-filled data as output
   cols.save <- c("date", "Q_mm.d", "HYSEP_fixed", "HYSEP_slide", "HYSEP_local", "UKIH", "BFLOW_1pass", "BFLOW_3pass", "Eckhardt")
@@ -186,9 +187,12 @@ for (cat in 1:n.catchment){
   
 }
 
+# add recessionConstant and BFImax to summary data frame
+df.summary.out <- left_join(df.summary.out, df.summary[,c("catchment", "recessionConstant", "BFImax")], by="catchment")
+df.summary.out[df.summary.out$method != "Eckhardt", c("recessionConstant", "BFImax")] <- NaN
+
 # Save summary -------------------------------------------------------------
 
 # save to GSAS and git repository
-write.csv(df.summary, file.path(dir.Q.derived, "Baseflow", "catchmentSummary_Baseflow.csv"), row.names=F)
-write.csv(df.summary, file.path("data", "catchmentSummary_Baseflow.csv"), row.names=F)
-
+write.csv(df.summary.out, file.path(dir.Q.derived, "Baseflow", "catchmentSummary.csv"), row.names=F)
+write.csv(df.summary.out, file.path("data", "catchmentSummary_Baseflow.csv"), row.names=F)
